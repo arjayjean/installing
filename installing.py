a82928e06da85a32cda1cf3d29d864eb7d2f27e9
Arjay Jean <thearjayjean@gmail.com>
2022-06-17 13:44:40 -0400

    Added the "Typer" library in order for the script to have a CLI interaction.

--- installing.py
+++ installing.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 import subprocess
+import typer
 
 subprocess.run('clear')
 
-install = input('What are you installing: ')
+app = typer.Typer()
 
-installation = ['python3', '-m', 'pip', 'install', install]
+def package(name):
+    return name
 
-subprocess.run(installation)
+def installing(package):
+    installation = ['python3', '-m', 'pip', 'install', f'{package}']
+
+    subprocess.run(installation)
+
+    subprocess.run('clear')
+@app.command()
+def pip(name: str):
+    installing(package(name))
+
+
+if __name__ == "__main__":
+    app()
